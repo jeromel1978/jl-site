5f3fead796420bdeca48705ddc15211db5c171c2
Jerome Losorata <jerome.losorata@hotmail.com>
2024-06-16 00:24:45 -0500

    minimal

--- src/app/blog/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Connect, Category } from "@/lib/upstash";
-const Blog = async () => {
-  const client = await Connect();
-  const Posts = await client.hGetAll(`${Category}`);
-  console.log(Posts);
-  return <div>Blog Main</div>;
-};
-export default Blog;
