84a39dba7267a3116efe0512008c335981e2a445
Jerome Losorata <jerome.losorata@hotmail.com>
2024-06-18 08:33:12 -0500

    Visuals tweaked

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Root() {
   return (
     <div className="w-full h-full">
       <Logo />
+      {/* <div className="flex absolute inset-x-0 inset-y-0 text-4xl text-white">Jerome Losorata</div> */}
     </div>
   );
 }
--- src/components/three/logo.tsx
+++ src/components/three/logo.tsx
@@ -2,54 +2,196 @@
 import * as THREE from "three";
 import React, { useRef, useState, useLayoutEffect } from "react";
 import { Canvas, useFrame, ThreeElements, useLoader } from "@react-three/fiber";
-import type { Mesh, BufferGeometry, NormalBufferAttributes, Material, Object3DEventMap } from "three";
-import { OrbitControls, useGLTF } from "@react-three/drei";
-
-type Logo3DProps = {
-  currentColor?: string;
-};
-export function Model(props: Logo3DProps) {
-  const { nodes, materials } = useGLTF("/models/Logo.glb");
-  const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap> | null>(
-    null
-  );
-  // const ref = useRef(null)
-  useFrame((state, delta) => {
-    if (!!ref?.current) ref.current.rotation.z += delta;
-  });
-  const ResetPosition = () => {
-    if (!!ref?.current) {
-      ref.current.rotation.x = 0;
-      ref.current.rotation.y = 0;
-    }
-  };
-  const LogoMesh = nodes.Logo as any;
-  return (
-    <group {...props} dispose={null}>
-      <mesh
-        ref={ref}
-        castShadow
-        receiveShadow
-        geometry={LogoMesh.geometry}
-        material={materials["Material.001"]}
-        rotation={[Math.PI / 2, 0, 0]}
-        // onPointerOut={ResetPosition}
-      />
-    </group>
-  );
-}
-useGLTF.preload("/models/Logo.glb");
+import type { Mesh, BufferGeometry, NormalBufferAttributes, Material, Object3DEventMap, Group } from "three";
+import {
+  useGLTF,
+  Center,
+  Caustics,
+  Environment,
+  Lightformer,
+  RandomizedLight,
+  PerformanceMonitor,
+  AccumulativeShadows,
+  MeshTransmissionMaterial,
+} from "@react-three/drei";
+import { easing } from "maath";
 
 const Logo = () => {
+  const [perfSucks, degrade] = useState(false);
   return (
-    <Canvas className="w-full h-full">
+    <Canvas
+      shadows
+      // className="w-full h-full"
+      dpr={[1, perfSucks ? 1.5 : 2]}
+      eventSource={document.getElementById("root") as HTMLElement}
+      eventPrefix="client"
+      camera={{ position: [20, 0.9, 20], fov: 26 }}
+    >
+      <PerformanceMonitor onDecline={() => degrade(true)} />
       <color attach="background" args={["black"]} />
-      <ambientLight intensity={Math.PI / 2} />
+      {/* <color attach="background" args={["#f0f0f0"]} /> */}
+      {/* <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Model />
-      <OrbitControls />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} /> */}
+
+      <group position={[0, -0.5, 0]} rotation={[0, 0, 0]}>
+        <Scene />
+        {/* <AccumulativeShadows
+          frames={100}
+          alphaTest={0.85}
+          opacity={0.8}
+          color="white"
+          scale={20}
+          position={[0, -0.005, 0]}
+        >
+          <RandomizedLight
+            amount={8}
+            radius={6}
+            ambient={0.5}
+            intensity={1}
+            position={[-1.5, 2.5, -2.5]}
+            bias={0.001}
+          />
+        </AccumulativeShadows> */}
+      </group>
+      <Env perfSucks={perfSucks} />
     </Canvas>
   );
 };
+
+export function Scene(props: any) {
+  const { nodes, materials } = useGLTF("/models/Logo.glb");
+  useGLTF.preload("/models/Logo.glb");
+  const [Clear, setClear] = useState(true);
+  // const ref = useRef<Mesh<BufferGeometry<NormalBufferAttributes>, Material | Material[], Object3DEventMap> | null>(
+  //   null
+  // );
+
+  // const ref = useRef(null)
+
+  // useFrame((state, delta) => {
+  //   if (!!ref?.current) ref.current.rotation.z += delta;
+  // });
+
+  const innerMaterial = new THREE.MeshStandardMaterial({
+    transparent: true,
+    opacity: 1,
+    color: "red",
+    roughness: 0,
+    side: THREE.FrontSide,
+    blending: THREE.AdditiveBlending,
+    polygonOffset: true,
+    polygonOffsetFactor: 1,
+    envMapIntensity: 2,
+  });
+
+  const LogoMesh = nodes.Logo as any;
+  return (
+    <group {...props} dispose={null}>
+      {!Clear && (
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={LogoMesh.geometry}
+          // material={materials["Material.001"]}
+          material={innerMaterial}
+          rotation={[Math.PI / 2, 0, 0]}
+          // onPointerOut={ResetPosition}
+          onClick={() => setClear(!Clear)}
+        />
+      )}
+      {Clear && (
+        <Caustics
+          color={[1, 0.8, 0.8]}
+          lightSource={[-1.2, 3, -2]}
+          intensity={0.003}
+          worldRadius={0.26 / 10}
+          ior={0.9}
+          causticsOnly={false}
+          backside={true}
+        >
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={LogoMesh.geometry}
+            rotation={[Math.PI / 2, 0, 0]}
+            onClick={() => setClear(!Clear)}
+          >
+            <MeshTransmissionMaterial
+              backside
+              backsideThickness={0.1}
+              thickness={0.05}
+              chromaticAberration={0.05}
+              anisotropicBlur={1}
+              clearcoat={1}
+              clearcoatRoughness={1}
+              envMapIntensity={2}
+            />
+          </mesh>
+        </Caustics>
+      )}
+    </group>
+  );
+}
+
+function Env({ perfSucks }: { perfSucks: boolean }) {
+  const ref = useRef<any | null>(null);
+  useFrame((state, delta) => {
+    // Animate the environment as well as the camera
+    if (!perfSucks) {
+      if (!!ref.current) {
+        easing.damp3(
+          ref.current.rotation as any,
+          [Math.PI / 2, 0, state.clock.elapsedTime / 5 + state.pointer.x],
+          0.2,
+          delta
+        );
+        easing.damp3(
+          state.camera.position,
+          [Math.sin(state.pointer.x / 4) * 9, 1.25 + state.pointer.y, Math.cos(state.pointer.x / 4) * 9],
+          0.5,
+          delta
+        );
+        state.camera.lookAt(0, 0, 0);
+      }
+    }
+  });
+  // Runtime environments can be too slow on some systems, better safe than sorry with PerfMon
+  return (
+    <Environment
+      frames={perfSucks ? 1 : Infinity}
+      // preset="city"
+      // resolution={1080}
+      // background blur={0.8}
+    >
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <Lightformer intensity={4} rotation-x={Math.PI / 2} position={[0, 5, -9]} scale={[10, 10, 1]} />
+      <group rotation={[Math.PI / 2, 1, 0]}>
+        {[2, -2, 2, -4, 2, -5, 2, -9].map((x, i) => (
+          <Lightformer
+            key={i}
+            intensity={1}
+            rotation={[Math.PI / 4, 0, 0]}
+            position={[x, 4, i * 4]}
+            scale={[4, 1, 1]}
+          />
+        ))}
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, 1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={Math.PI / 2} position={[-5, -1, -1]} scale={[50, 2, 1]} />
+        <Lightformer intensity={0.5} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={[50, 2, 1]} />
+      </group>
+      <group ref={ref}>
+        <Lightformer
+          intensity={5}
+          form="ring"
+          color="red"
+          rotation-y={Math.PI / 2}
+          position={[-5, 2, -1]}
+          scale={[10, 10, 1]}
+        />
+      </group>
+    </Environment>
+  );
+}
+
 export default Logo;
